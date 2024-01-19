f205adfee839562a32985590eadf302db463b7c2
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-01-19 17:10:58 +0300

    setting up student login

--- Nebula/nebulaapp/views.py
+++ Nebula/nebulaapp/views.py
@@ -9,7 +9,7 @@ from django.db.models import Q
 
 user_cache = {}
 
-async def fetch_user(session, email ):
+async def fetch_student(session, email ):
     user_url = f'https://labmero.com/nebula_server/api/student/{email}'
     async with session.get(user_url) as response:
         if response.status == 200:
@@ -18,4 +18,39 @@ async def fetch_user(session, email ):
             user_cache[email] = user_data
 
 
+async def fetch_students():
+    url = f'https://labmero.com/nebula_server/api/students'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response == 200:
+                students = await response.json()
+
+
+
+def login(request):
+    if request.method == "POST":
+        ## We will use the email as the username and the cohort as the password
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        students_url = "https://labmero.com/nebula_server/api/students"
+        response = requests.get(students_url)
+        students_data = response.json()
+
+        ## Finding users with a matching email address and a cohort name
+        authenticated_student = None
+        for student in students_data:
+            if student['email'] == username and student['cohort'] == password:
+                authenticated_student = student
+                break
+        if authenticated_student:
+            return JsonResponse({'student': authenticated_student}, safe=False)
+        
+        else:
+            return JsonResponse({'student': '0'}, safe=False)
+    return render(request, 'index.html')
+
+
+
+    
 
