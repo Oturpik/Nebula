be76b05c05a2d31873cb47680b90a680393b1f0a
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-01-19 16:52:45 +0300

    getting and caching student data

--- Nebula/nebulaapp/views.py
+++ Nebula/nebulaapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+import aiohttp
+import asyncio
+from django.http import JsonResponse, HttpResponse
+from django.db.models import Q
 
 # Create your views here.
+
+user_cache = {}
+
+async def fetch_user(session, email ):
+    user_url = f'https://labmero.com/nebula_server/api/student/{email}'
+    async with session.get(user_url) as response:
+        if response.status == 200:
+            user_data = await response.json()
+            # saving user data in cache for later retrieval
+            user_cache[email] = user_data
+
+
+
