57b8c19fbaae01318a2e667a46f5588beff7fcf0
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-01-25 15:03:15 +0300

    testing that all endpoints are working as desired

--- Nebula/nebulaapp/urls.py
+++ Nebula/nebulaapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'), 
-    #path('login/', views.login, name='login'),
+    path('api/login/', views.login, name='login'),
     path('api/health-check', views.fetch_health_check, name='health_check'),
     path('api/test-db-connection', views.fetch_dbconnection_check, name='db_check'),
     path('api/students', views.fetch_students, name='all_students_list'), 
