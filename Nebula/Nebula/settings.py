f76ee0760687fdc65dcf855e86a4b0406f06ab2e
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-01-31 16:54:13 +0300

    provisioning for deployment to container

--- Nebula/Nebula/settings.py
+++ Nebula/Nebula/settings.py
@@ -94,12 +94,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # MySQL DB  Configuration
 # DATABASES = {
@@ -113,16 +113,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 # }}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nebula',
-        'USER': 'postgres',
-        'PASSWORD': 'c3po',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nebula',
+#         'USER': 'postgres',
+#         'PASSWORD': 'c3po',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
